9e1ec14844faf59298b58cf8278d6a5f3146b226
Callum Leslie <github@cleslie.uk>
2022-05-18 20:43:41 +0100

    Fix formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 use std::fmt;
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
+/// use `wee_alloc` as the global allocator.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
